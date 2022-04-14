248f4ae8f021b56fa97dfcfbbde3997b93b26041
zillur-rgb <zillurdeu@gmail.com>
2022-04-14 04:41:33 +0200

    Basic setup and Sidebar

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Pages from "./Pages/Pages";
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <Pages />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <p className="logo">Invoice Master</p>
+    </div>
+  );
+};
+
+export default Sidebar;
